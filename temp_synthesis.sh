a8eaf5e48ec2bb004dd161a1f7703ab2178a87f8
ander1119 <b07902060@ntu.edu.tw>
2021-06-08 02:22:40 +0800

    produce result

--- temp_synthesis.sh
+++ temp_synthesis.sh
@@ -1,5 +1,6 @@
-/Users/qiuyuyang/Desktop/class-3-2/DIP/proposal/code/ebsynth/bin/ebsynth \
-    -style inputImage/styleImage/style2.png \
-    -guide inputImage/styleImage/G_app/style2.png inputImage/targetImage/G_app/target2.png \
-    -weight 2.0 -guide inputImage/styleImage/segmentation_images/style2.png inputImage/targetImage/segmentation_images/target2.png \
-    -weight 1.5 -guide inputImage/styleImage/G_pos/style2.png inputImage/targetImage/G_pos/target2.png -weight 1.5 -output output.png
+./ebsynth/bin/ebsynth \
+    -style inputImage/styleImage/$1.png \
+    -guide inputImage/styleImage/G_app/$1.png inputImage/targetImage/G_app/$2.png -weight 2.0 \
+    -guide inputImage/styleImage/segmentation_images/$1.png inputImage/targetImage/segmentation_images/$2.png -weight 1.5 \
+    -guide inputImage/styleImage/G_pos/$1.png inputImage/targetImage/G_pos/$2.png -weight 1.5 \
+    -output result/$1_$2.png
