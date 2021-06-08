4f51906dfa119a473f0b2212f0c82c1459a09a36
ander1119 <b07902060@ntu.edu.tw>
2021-06-08 11:39:59 +0800

    comment G_pos

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 
 ## Get G_seg
-python3 ./code/Gseg/segment.py  --input_dir ./inputImage/styleImage  --conf_threshold 0.99
-python3 ./code/Gseg/segment.py  --input_dir ./inputImage/targetImage --conf_threshold 0.99
+# python3 ./code/Gseg/segment.py  --input_dir ./inputImage/styleImage  --conf_threshold 0.99
+# python3 ./code/Gseg/segment.py  --input_dir ./inputImage/targetImage --conf_threshold 0.99
 
 ## Get G_pos
-python3 code/Gpos/mlsAffineMorpher.py ./inputImage/styleImage style2.png ./inputImage/targetImage target2.png
+python3 code/Gpos/mlsAffineMorpher.py ./inputImage/styleImage $1.png ./inputImage/targetImage $2.png
 
 ## Get G_app
-python3 code/Gapp/generateGapp.py  ./inputImage/styleImage style2.png ./inputImage/targetImage target2.png
+python3 code/Gapp/generateGapp.py  ./inputImage/styleImage $1.png ./inputImage/targetImage $2.png
